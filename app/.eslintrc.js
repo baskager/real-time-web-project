53f58bf8d8c9f3736ee4c084cb45812840f18f3d
Bas Kager <bas@kager.io>
2018-04-26 11:32:30 +0200

    Base app, closes #2

---
+++ app/.eslintrc.js
@@ -0,0 +1,27 @@
+module.exports = {
+    "env": {
+        "browser": true,
+        "commonjs": true,
+        "es6": true
+    },
+    "extends": "eslint:recommended",
+    "rules": {
+        "indent": [
+            "error",
+            4
+        ],
+        "linebreak-style": [
+            "error",
+            "unix"
+        ],  
+        "no-console": "off",
+        "quotes": [
+            "error",
+            "double"
+        ],
+        "semi": [
+            "error",
+            "always"
+        ]
+    }
+};
---
+++ app/index.js
@@ -0,0 +1,86 @@
+const express = require("express"),
+    app = express(),
+    http = require("http").Server(app),
+    debug = require("debug")("remind-server"),
+    io = require("socket.io")(http),
+    handlebars  = require("express-handlebars"),
+    config = require("./config"),
+    port = config.port || 3000,
+    db = require("arangojs")();
+
+db.useBasicAuth("root", "openSesame");
+db.useDatabase("reminders");
+const collection = db.collection("chat");
+
+app.engine("handlebars", handlebars({defaultLayout: "main"}));
+app.set("view engine", "handlebars");
+
+// Define directory from which static files are served
+app.use(express.static("public"));
+
+app.get("/reminders", function(req, res) {
+    res.render("reminders");
+});
+
+
+app.get("/", function(req, res) {
+    res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", function(socket) {
+    debug("A user connected");
+    io.emit("chat message", "Server: You are connected");
+
+    collection.all().then(
+        cursor => cursor.map(doc => doc)
+    ).then(
+        docs => {
+            for(let doc of docs) {
+                io.emit("chat message", doc.message);
+            }
+        },
+        err => io.emit("chat message", "Loading chat history failed: " + err.message)
+    ); 
+
+    socket.on("chat message", function(msg){
+        let doc = {
+            timestamp: new Date(),
+            message: msg
+        };
+        collection.save(doc).then(
+            () => io.emit("chat message", doc.message),
+            err =>  io.emit("chat message", "Failed to send message. Error: " + err)
+        );
+    });
+
+    socket.on("disconnect", function(){
+        debug("A user disconnected");
+    });
+});
+
+// app.get("/createdb", function(req, res) {
+//     debug("creating new database");
+//     debug(db);
+
+//     db.createDatabase("reminders").then(  () => {
+//         debug("database created");
+//         res.send("success");
+//     }, err => {
+//         debug("Failed to create database: " + err);
+//         res.send("failed");
+//     });
+
+// });
+
+// app.get("/createcollection", function(req, res) {
+//     collection.create().then(
+//         () => console.log("Collection created"),
+//         err => console.error("Failed to create collection: ", err)
+//     );
+
+//     res.send(db);
+// });
+
+http.listen(port, function(){
+    console.log("Server listening on port " + port);
+});
