657e40a9c28990899a248dfb18db7a4632fc5403
Bas Kager <baskager@hotmail.com>
2019-05-16 11:23:52 +0200

    Socket: Separate front-end socket implementation from the view

---
+++ app/public/static/js/socket.js
@@ -0,0 +1,10 @@
+/**
+* Live search filter
+*
+* @author  Bas Kager
+* @version 1.1
+* @since   13-02-2019
+*/
+(function () {
+
+})();
---
+++ app/src/js/reminder-socket-ui.js
@@ -0,0 +1,35 @@
+/**
+* Socket implementation for the User Interface
+*
+* @author  Bas Kager
+* @version 1.0
+* @since   16-05-2019
+*/
+(function () {
+  var socket = io("127.0.0.1:1511");
+  var card = document.getElementById("reminderCardCopy");
+  var cardCarousel = document.getElementById("cardCarousel");
+  
+  socket.on("reminder", function(reminder){
+      var cardClone = card.cloneNode(true);
+      // Define which entries of the thumbnail should be searched
+      var titleField = cardClone.querySelector("#title");
+      var postedByField = cardClone.querySelector("#name");
+      var datePostedField = cardClone.querySelector("#datePosted");
+      var dateDueField = cardClone.querySelector("#dateDue");
+      var avatarField = cardClone.querySelector("#avatar");
+
+      titleField.innerHTML = reminder.title;
+      postedByField.innerHTML = reminder.authorName;
+      datePostedField.innerHTML = moment(reminder.timestamp).format("DD-MM-YYYY");
+      if(reminder.due) dateDueField.innerHTML = moment(reminder.due).format("DD-MM-YYYY");
+      avatarField.src = "https://cdn.discordapp.com/avatars/"+reminder.authorId+"/"+reminder.avatar+".png?size=256"
+
+      cardCarousel.appendChild(cardClone);
+      setTimeout(function () {
+          cardClone.classList.remove("hide")
+          cardClone.classList.remove("faded")
+      },50);
+  
+  });
+})();
